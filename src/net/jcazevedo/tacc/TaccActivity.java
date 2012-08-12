84205679b0e9bab51f32f57a415f358a715fca17
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-08-12 19:05:27 +0100

    Add some boilerplate code.

--- src/net/jcazevedo/tacc/TaccActivity.java
+++ src/net/jcazevedo/tacc/TaccActivity.java
@@ -9,7 +9,6 @@ public class TaccActivity extends Activity {
     private ToggleButton whitePlayerButton;
     private ToggleButton blackPlayerButton;
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +17,14 @@ public class TaccActivity extends Activity {
 
         whitePlayerButton = (ToggleButton) findViewById(R.id.white_player_button);
         blackPlayerButton = (ToggleButton) findViewById(R.id.black_player_button);
+    }
+
+    private void pause() {
+    }
+
+    private void toggle(ToggleButton buttonPressed) {
+    }
+
+    private void reset() {
     }
 }
