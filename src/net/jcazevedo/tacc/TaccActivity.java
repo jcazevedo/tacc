dfc721e68cb47a0dcd41305560d598bdbd419987
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-08-12 18:55:20 +0100

    Add initial layout.

--- src/net/jcazevedo/tacc/TaccActivity.java
+++ src/net/jcazevedo/tacc/TaccActivity.java
@@ -2,12 +2,21 @@ package net.jcazevedo.tacc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ToggleButton;
 
 public class TaccActivity extends Activity {
+    ToggleButton whitePlayerButton;
+    ToggleButton blackPlayerButton;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+
+        whitePlayerButton = (ToggleButton) findViewById(R.id.white_player_button);
+        blackPlayerButton = (ToggleButton) findViewById(R.id.black_player_button);
     }
 }
